56209c3d06dd1a08f5ea2f2459f2f677a777fe43
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2024-11-22 19:43:17 -0300

    erro de importação do core 'contornado', mudando para a importação relativa

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -1,19 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { eventos } from 'core';
 
 async function seed() {
   const prisma = new PrismaClient();
-  
-  eventos.forEach(async (evento) =>
-    {
-    await prisma.evento.create({
-  data: {
-    id: evento.id,
-    alias: evento.alias,
-    
-  }
-    })
 
-  }
+  eventos.forEach(async (evento) => {
+    await prisma.evento.create({
+      data: evento,
+    });
+  });
 }
 
 seed();
